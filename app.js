89b39aa0ecb3803e9b0351fa893fe90a9fcc21df
Anton Synenko <anthony.synenko@gmail.com>
2020-04-11 17:14:19 +0300

    21. ng-if ng-hide ng-show ng-class ng-for directives

--- app.js
+++ app.js
@@ -10,8 +10,17 @@ angularApp.controller('mainController', [
   '$filter',
   function ($scope, $filter) {
     $scope.handle = '';
+
     $scope.lowerCaseHandle = function () {
-      return $filter('lowercase')($scope.handle)
+      return $filter('lowercase')($scope.handle);
     }
+
+    $scope.characters = 5;
+
+    $scope.rules = [
+      { rulename: "Must be 5 characters" },
+      { rulename: "Must not be used elsewhere" },
+      { rulename: "Must be cool" },
+    ];
   },
 ]);
