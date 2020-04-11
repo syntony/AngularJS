b5eeac6684b95452263ff47f3c69b7f8af2d02c4
Anton Synenko <anthony.synenko@gmail.com>
2020-04-11 17:39:34 +0300

    24. External Data and $http (scoped version of axios)

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var angularApp = angular.module('angularApp', [
 angularApp.controller('mainController', [
   '$scope',
   '$filter',
-  function ($scope, $filter) {
+  '$http',
+  function ($scope, $filter, $http) {
     $scope.handle = '';
 
     $scope.lowerCaseHandle = function () {
@@ -17,10 +18,32 @@ angularApp.controller('mainController', [
 
     $scope.characters = 5;
 
+    /*
     $scope.rules = [
       { rulename: "Must be 5 characters" },
       { rulename: "Must not be used elsewhere" },
       { rulename: "Must be cool" },
     ];
+     */
+    $http.get('/api')
+      .success(function(result) {
+        $scope.rules = result;
+      })
+      .error(function(data, status) {
+        console.error(data);
+      })
+
+    $scope.newRule = '';
+
+    $scope.addRule = function() {
+        $http.post('/api', { newRule: $scope.newRule })
+          .success(function(result) {
+            $scope.rules = result;
+            $scope.newRule = '';
+          })
+          .error(function(data, status) {
+            console.error(data);
+          })
+    };
   },
 ]);
