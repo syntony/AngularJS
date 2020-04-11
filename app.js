957fe28b6d2c4899df8cc00630a4341f3de0ae9d
Anton Synenko <anthony.synenko@gmail.com>
2020-04-11 18:52:40 +0300

    30. Custom Services

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
 // MODULE
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngRoute']);
+
+angularApp.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/main.html',
+      controller: 'mainController',
+    })
+    .when('/second', {
+      templateUrl: 'pages/second.html',
+      controller: 'secondController',
+    })
+    .when('/second/:num', {
+      templateUrl: 'pages/second.html',
+      controller: 'secondController',
+    })
+});
+
+angularApp.service('nameService', function() {
+  var self = this;
+  this.name = 'John Doe';
+  this.namelength = function() {
+    return self.name.length;
+  }
+});
 
 // CONTROLLERS
 angularApp.controller('mainController', [
   '$scope',
-  function ($scope) {
+  '$log',
+  'nameService',
+  function ($scope, $log, nameService) {
+    $scope.name = nameService.name;
+    $scope.$watch('name', function() {
+      nameService.name = $scope.name;
+    })
+  }
+]);
 
+angularApp.controller('secondController', [
+  '$scope',
+  '$log',
+  '$routeParams',
+  'nameService',
+  function ($scope, $log, $routeParams, nameService) {
+    $scope.num = $routeParams.num || 1;
+    $scope.name = nameService.name;
+    $scope.$watch('name', function() {
+      nameService.name = $scope.name;
+    })
   }
 ]);
