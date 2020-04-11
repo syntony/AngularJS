8a94d4b3743f49be3b2f43f7e3cc49b6366cf0d9
Anton Synenko <anthony.synenko@gmail.com>
2020-04-11 18:14:09 +0300

    starter app

--- app.js
+++ app.js
@@ -1,49 +1,10 @@
 // MODULE
-var angularApp = angular.module('angularApp', [
-  'ngMessages',
-  'ngResource',
-]);
+var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
 angularApp.controller('mainController', [
   '$scope',
-  '$filter',
-  '$http',
-  function ($scope, $filter, $http) {
-    $scope.handle = '';
+  function ($scope) {
 
-    $scope.lowerCaseHandle = function () {
-      return $filter('lowercase')($scope.handle);
-    }
-
-    $scope.characters = 5;
-
-    /*
-    $scope.rules = [
-      { rulename: "Must be 5 characters" },
-      { rulename: "Must not be used elsewhere" },
-      { rulename: "Must be cool" },
-    ];
-     */
-    $http.get('/api')
-      .success(function(result) {
-        $scope.rules = result;
-      })
-      .error(function(data, status) {
-        console.error(data);
-      })
-
-    $scope.newRule = '';
-
-    $scope.addRule = function() {
-        $http.post('/api', { newRule: $scope.newRule })
-          .success(function(result) {
-            $scope.rules = result;
-            $scope.newRule = '';
-          })
-          .error(function(data, status) {
-            console.error(data);
-          })
-    };
-  },
+  }
 ]);
