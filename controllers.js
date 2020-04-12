d0c7f1af7611526959e2f6ac52c11dad38b06f7a
Anton Synenko <anthony.synenko@gmail.com>
2020-04-12 16:44:52 +0300

    upd app

--- controllers.js
+++ controllers.js
@@ -2,41 +2,35 @@ var openWeatherMapKey = process.env.OPEN_WEATHER_MAP_API_KEY || '';
 // CONTROLLERS
 weatherApp.controller('homeController', [
   '$scope',
+  '$location',
   'cityService',
-  function ($scope, cityService) {
+  function ($scope, $location, cityService) {
     $scope.city = cityService.city;
     $scope.$watch('city', function() {
       cityService.city = $scope.city;
     });
+
+    $scope.submit = function() {
+      $location.path('/forecast');
+    }
   }
 ]);
 weatherApp.controller('forecastController', [
   '$scope',
-  '$resource',
   '$routeParams',
   'cityService',
+  'weatherService',
   function (
     $scope,
-    $resource,
     $routeParams,
-    cityService
+    cityService,
+    weatherService,
   ) {
     $scope.city = cityService.city;
     $scope.days = $routeParams.days || '2';
 
-    $scope.weatherAPI =
-      $resource(
-        'http://api.openweathermap.org/data/2.5/forecast?APPID=' + openWeatherMapKey,
-        { callback: 'JSON_CALLBACK' },
-        { get: { method: 'JSONP' } },
-      );
-
     $scope.weatherResult =
-      $scope.weatherAPI.get({
-        q: $scope.city,
-        cnt: $scope.days,
-        units: 'metric',
-      });
+      weatherService.GetWeather($scope.city, $scope.days);
 
     $scope.convertToDate = function(dt) {
       return new Date(dt * 1000);
--- services.js
+++ services.js
@@ -2,3 +2,24 @@
 weatherApp.service('cityService', function() {
   this.city = 'New York';
 });
+
+weatherApp.service('weatherService', [
+  '$resource',
+  function($resource) {
+    this.GetWeather = function (city, days) {
+      var weatherAPI =
+        $resource(
+          'http://api.openweathermap.org/data/2.5/forecast?APPID=' + openWeatherMapKey,
+          {callback: 'JSON_CALLBACK'},
+          {get: {method: 'JSONP'}},
+        );
+
+      return weatherResult =
+        weatherAPI.get({
+          q: city,
+          cnt: days,
+          units: 'metric',
+        });
+    };
+  },
+]);
